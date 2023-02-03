29e5c419f3ead4a86fd828ab4150df85876dad30
barnum <eaddress27@gmail.com>
2023-02-03 17:53:51 +0200

    Modify func

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ def main_function():
     lower_bound = 1
     to_be_guessed = random.randint(lower_bound, upper_bound)
     
+    for i in range(6):
+        guess = int(input("нове число: "))    
+        if guess < lower_bound or guess > upper_bound:
+            print("число за межами діапазону!")
+            continue
+        if guess == to_be_guessed:
+            print('число вгадане з {} спроби'.format(i+1))
+            break
+        elif guess < to_be_guessed:
+            print('ваше число меньше завданого')                
+            lower_bound = guess + 1
+        elif guess > to_be_guessed:
+            print('ваше число більше завданого')                
+            upper_bound = guess - 1
