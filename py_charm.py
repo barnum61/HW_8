38bd56440ab32528dbb1c19b4986f5e915f5dd03
barnum <eaddress27@gmail.com>
2023-02-03 19:59:29 +0200

    Add func to PyCharm file

--- py_charm.py
+++ py_charm.py
@@ -13,3 +13,9 @@ seasons = {(12, 1, 2): "зима",
              (3, 4, 5): "весна!",
              (6, 7, 8): "літо",
              (9, 10, 11): "осінь"}
+
+def w_file(file_name, dc):
+
+    with open(file_name, 'w') as file:
+        for key, value in dc.items():
+            file.write('{0}: {1}\n'.format(key, value))
